b8a63bfa3ae460e24defe6817ac32d622cb1e07e
yanghao <yanghao@qingsongchou.com>
2018-02-01 16:39:23 +0800

    //适配器模式

---
+++ app/src/adapter/abstractadapter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18-2-1
+ * Time: 下午4:34
+ */
+abstract class abstractadapter implements classinterface {
+    public function method()
+    {
+        // TODO: Implement method() method.
+    }
+    public function action()
+    {
+        // TODO: Implement action() method.
+    }
+}
---
+++ app/src/adapter/classsource.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18-2-1
+ * Time: 下午4:15
+ */
+class classsource{
+    public function method(){
+        echo '类适配器';
+    }
+}
